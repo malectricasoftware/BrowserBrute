488438778d7542b5fb8dc528bc0b62934404e04f
witchdocsec <107813117+witchdocsec@users.noreply.github.com>
2024-05-16 13:09:34 +0100

    Update browserbrute.py

--- browserbrute.py
+++ browserbrute.py
@@ -1,8 +1,13 @@
+#imports
 import lib.brute
 import lib.parse
 import lib.banner
 
+#parse arguments
 args=lib.parse.parser()
+
+#print banner
 lib.banner.banner()
 
+#online bruteforce
 lib.brute.brute(args)
